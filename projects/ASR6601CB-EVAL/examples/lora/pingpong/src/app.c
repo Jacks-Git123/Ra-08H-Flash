d1788ce12ebf6951bf2191024a18c887e2f2f90b
Jacks-Git123 <chewie.jewison@gmail.com>
2026-05-21 20:38:24 -0500

    Add files via upload

---
+++ projects/ASR6601CB-EVAL/examples/lora/pingpong/src/app.c
@@ -0,0 +1,300 @@
+/*!
+ * \file      main.c
+ *
+ * \brief     Ping-Pong implementation
+ *
+ * \copyright Revised BSD License, see section \ref LICENSE.
+ *
+ * \code
+ *                ______                              _
+ *               / _____)             _              | |
+ *              ( (____  _____ ____ _| |_ _____  ____| |__
+ *               \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ *               _____) ) ____| | | || |_| ____( (___| | | |
+ *              (______/|_____)_|_|_| \__)_____)\____)_| |_|
+ *              (C)2013-2017 Semtech
+ *
+ * \endcode
+ *
+ * \author    Miguel Luis ( Semtech )
+ *
+ * \author    Gregory Cristian ( Semtech )
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "delay.h"
+#include "timer.h"
+#include "radio.h"
+#include "tremo_system.h"
+
+#define REGION_US915
+#define USE_MODEM_LORA
+
+#if defined( REGION_AS923 )
+
+#define RF_FREQUENCY                                923000000 // Hz
+
+#elif defined( REGION_AU915 )
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#elif defined( REGION_CN470 )
+
+#define RF_FREQUENCY                                470000000 // Hz
+
+#elif defined( REGION_CN779 )
+
+#define RF_FREQUENCY                                779000000 // Hz
+
+#elif defined( REGION_EU433 )
+
+#define RF_FREQUENCY                                433000000 // Hz
+
+#elif defined( REGION_EU868 )
+
+#define RF_FREQUENCY                                868000000 // Hz
+
+#elif defined( REGION_KR920 )
+
+#define RF_FREQUENCY                                920000000 // Hz
+
+#elif defined( REGION_IN865 )
+
+#define RF_FREQUENCY                                865000000 // Hz
+
+#elif defined( REGION_US915 )
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#elif defined( REGION_US915_HYBRID )
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#else
+    #error "Please define a frequency band in the compiler options."
+#endif
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#if defined( USE_MODEM_LORA )
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+#elif defined( USE_MODEM_FSK )
+
+#define FSK_FDEV                                    25000     // Hz
+#define FSK_DATARATE                                50000     // bps
+#define FSK_BANDWIDTH                               50000   // Hz >> DSB in sx126x
+#define FSK_AFC_BANDWIDTH                           83333   // Hz
+#define FSK_PREAMBLE_LENGTH                         5         // Same for Tx and Rx
+#define FSK_FIX_LENGTH_PAYLOAD_ON                   false
+
+#else
+    #error "Please define a modem in the compiler options."
+#endif
+
+typedef enum
+{
+    LOWPOWER,
+    RX,
+    RX_TIMEOUT,
+    RX_ERROR,
+    TX,
+    TX_TIMEOUT
+}States_t;
+
+#define RX_TIMEOUT_VALUE                            1800
+#define BUFFER_SIZE                                 128 // Define the payload size here
+
+extern volatile uint8_t rx_data[RX_SIZE];
+extern volatile uint16_t rx_index;
+
+uint16_t BufferSize = BUFFER_SIZE;
+uint8_t Buffer[BUFFER_SIZE];
+
+#define UART_BUF_SIZE 256
+uint8_t uartBuf[UART_BUF_SIZE];
+volatile uint16_t uartLen = 0;
+volatile uint8_t uartReady = 0;
+uint8_t byte;
+
+volatile uint8_t txBusy = 0;
+
+volatile States_t State = LOWPOWER;
+
+int8_t RssiValue = 0;
+int8_t SnrValue = 0;
+
+uint32_t ChipId[2] = {0};
+
+/*!
+ * Radio events function pointer
+ */
+static RadioEvents_t RadioEvents;
+
+/*!
+ * \brief Function to be executed on Radio Tx Done event
+ */
+void OnTxDone( void );
+
+/*!
+ * \brief Function to be executed on Radio Rx Done event
+ */
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr );
+
+/*!
+ * \brief Function executed on Radio Tx Timeout event
+ */
+void OnTxTimeout( void );
+
+/*!
+ * \brief Function executed on Radio Rx Timeout event
+ */
+void OnRxTimeout( void );
+
+/*!
+ * \brief Function executed on Radio Rx Error event
+ */
+void OnRxError( void );
+
+void UartWrite(uint8_t *data, uint16_t len) {
+    for (uint16_t i = 0; i < len; i++) {
+        lpuart_send_data(LPUART, data[i]);
+        while (!lpuart_get_tx_done_status(LPUART));
+        lpuart_clear_tx_done_status(LPUART);
+    }
+}
+
+/**
+ * Main application entry point.
+ */
+
+int app_start(void) {
+    uint8_t i;
+    uint32_t random;
+
+    (void)system_get_chip_id(ChipId);
+    
+    // Radio initialization
+    RadioEvents.TxDone = OnTxDone;
+    RadioEvents.RxDone = OnRxDone;
+    RadioEvents.TxTimeout = OnTxTimeout;
+    RadioEvents.RxTimeout = OnRxTimeout;
+    RadioEvents.RxError = OnRxError;
+
+    Radio.Init( &RadioEvents );
+
+    Radio.SetChannel( RF_FREQUENCY );
+
+#if defined( USE_MODEM_LORA )
+
+    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
+
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                                   LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                                   LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+
+#elif defined( USE_MODEM_FSK )
+
+    Radio.SetTxConfig( MODEM_FSK, TX_OUTPUT_POWER, FSK_FDEV, 0,
+                                  FSK_DATARATE, 0,
+                                  FSK_PREAMBLE_LENGTH, FSK_FIX_LENGTH_PAYLOAD_ON,
+                                  true, 0, 0, 0, 3000 );
+
+    Radio.SetRxConfig( MODEM_FSK, FSK_BANDWIDTH, FSK_DATARATE,
+                                  0, FSK_AFC_BANDWIDTH, FSK_PREAMBLE_LENGTH,
+                                  0, FSK_FIX_LENGTH_PAYLOAD_ON, 0, true,
+                                  0, 0,false, true );
+
+#else
+    #error "Please define a frequency band in the compiler options."
+#endif
+
+    Radio.Rx( RX_TIMEOUT_VALUE );
+
+    while (1) {
+		Radio.IrqProcess();
+
+		if (rx_index > 0 && !txBusy) {
+			int pktLen = -1;
+
+			// Briefly disable interrupts to scan the buffer safely
+			NVIC_DisableIRQ(LPUART_IRQn); 
+			for (int i = 0; i < rx_index; i++) {
+				if (rx_data[i] == '\n') {
+					pktLen = i + 1;
+					break;
+				}
+			}
+
+			if (pktLen > 0) {
+				txBusy = 1;
+				Radio.Send((uint8_t *)rx_data, pktLen);
+				
+				uint16_t remaining = rx_index - pktLen;
+				if (remaining > 0) {
+					memmove((void*)rx_data, (void*)&rx_data[pktLen], remaining);
+				}
+				rx_index = remaining;
+			}
+			NVIC_EnableIRQ(LPUART_IRQn);
+		}
+	}
+}
+
+void OnTxDone(void) {
+    Radio.Sleep( );
+	uartLen = 0;
+	txBusy = 0;
+	State = LOWPOWER;
+	Radio.Rx(RX_TIMEOUT_VALUE);
+}
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
+    Radio.Sleep( );
+    BufferSize = size;
+    memcpy( Buffer, payload, BufferSize );
+	UartWrite(Buffer, BufferSize);
+	
+    RssiValue = rssi;
+    SnrValue = snr;
+	
+    State = LOWPOWER;
+	Radio.Rx(RX_TIMEOUT_VALUE);
+}
+
+void OnTxTimeout(void) {
+    Radio.Sleep( );
+    State = LOWPOWER; //TX_TIMEOUT
+	Radio.Rx(RX_TIMEOUT_VALUE);
+}
+
+void OnRxTimeout(void) {
+    //printf("OnRxTimeout\r\n");
+    Radio.Sleep( );
+    State = LOWPOWER; //RX_TIMEOUT
+	Radio.Rx(RX_TIMEOUT_VALUE);
+}
+
+void OnRxError(void) {
+    Radio.Sleep( );
+    State = LOWPOWER; //RX_ERROR
+	Radio.Rx(RX_TIMEOUT_VALUE);
+}
--- projects/ASR6601CB-EVAL/examples/lora/pingpong/src/main.c
+++ projects/ASR6601CB-EVAL/examples/lora/pingpong/src/main.c
@@ -3,35 +3,44 @@
 #include "delay.h"
 #include "timer.h"
 #include "radio.h"
+#include "tremo_regs.h"
 #include "tremo_uart.h"
+#include "tremo_lpuart.h"
 #include "tremo_gpio.h"
 #include "tremo_rcc.h"
 #include "tremo_pwr.h"
 #include "tremo_delay.h"
 #include "rtc-board.h"
 
+#define RX_SIZE 256
+
 extern int app_start(void);
 
-void uart_log_init(void)
-{
-    // uart0
-    gpio_set_iomux(GPIOB, GPIO_PIN_0, 1);
-    gpio_set_iomux(GPIOB, GPIO_PIN_1, 1);
+//LPUART Configuration
+void LpuartInit(void);
+void lpuart_IRQHandler(void);
 
-    /* uart config struct init */
-    uart_config_t uart_config;
-    uart_config_init(&uart_config);
+//void uart_log_init(void)
+//{
+    //// uart0
+    //gpio_set_iomux(GPIOB, GPIO_PIN_0, 1);
+    //gpio_set_iomux(GPIOB, GPIO_PIN_1, 1);
 
-    uart_config.baudrate = UART_BAUDRATE_115200;
-    uart_init(CONFIG_DEBUG_UART, &uart_config);
-    uart_cmd(CONFIG_DEBUG_UART, ENABLE);
-}
+    ///* uart config struct init */
+    //uart_config_t uart_config;
+    //uart_config_init(&uart_config);
 
-void board_init()
-{
+    //uart_config.baudrate = UART_BAUDRATE_115200;
+    //uart_init(CONFIG_DEBUG_UART, &uart_config);
+    //uart_cmd(CONFIG_DEBUG_UART, ENABLE);
+//}
+
+void board_init() {
+	rcc_enable_peripheral_clk(RCC_PERIPHERAL_AFEC, true);
     rcc_enable_oscillator(RCC_OSC_XO32K, true);
 
-    rcc_enable_peripheral_clk(RCC_PERIPHERAL_UART0, true);
+    //rcc_enable_peripheral_clk(RCC_PERIPHERAL_UART0, true);
+	rcc_enable_peripheral_clk(RCC_PERIPHERAL_LPUART, true);
     rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOA, true);
     rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOB, true);
     rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOC, true);
@@ -44,22 +53,69 @@ void board_init()
     delay_ms(100);
     pwr_xo32k_lpm_cmd(true);
     
-    uart_log_init();
+    //uart_log_init();
+	LpuartInit();
 
     RtcInit();
 }
 
-int main(void)
-{
+volatile uint8_t rx_data[RX_SIZE];
+volatile uint16_t rx_index = 0;
+//uint8_t tx_data[11] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' };
+
+int main(void) {
     // Target board initialization
     board_init();
 
     app_start();
 }
 
+void LpuartInit(void) {
+	lpuart_init_t lpuart_init_cofig;
+
+	gpio_set_iomux(GPIOC, GPIO_PIN_11, 2); // TX:GP11
+	gpio_set_iomux(GPIOD, GPIO_PIN_10, 2); // RX:GP10
+
+	lpuart_init_cofig.baudrate         = 9600;
+	lpuart_init_cofig.data_width       = LPUART_DATA_8BIT;
+	lpuart_init_cofig.parity           = LPUART_PARITY_NONE;
+	lpuart_init_cofig.stop_bits        = LPUART_STOP_1BIT;
+	lpuart_init_cofig.low_level_wakeup = true;
+	lpuart_init_cofig.start_wakeup     = false;
+	lpuart_init_cofig.rx_done_wakeup   = false;
+	lpuart_init(LPUART, &lpuart_init_cofig);
+
+	lpuart_config_interrupt(LPUART, LPUART_CR1_RX_NOT_EMPTY_INT, ENABLE);
+
+	lpuart_config_tx(LPUART, true);
+	lpuart_config_rx(LPUART, true);
+
+	/* NVIC config */
+	NVIC_SetPriority(LPUART_IRQn, 2);
+	NVIC_EnableIRQ(LPUART_IRQn);
+
+	//for (int i = 0; i < 11; i++) {
+		//lpuart_send_data(LPUART, tx_data[i]);
+		//while (!lpuart_get_tx_done_status(LPUART)) ;
+		//lpuart_clear_tx_done_status(LPUART);
+	//}
+}
+
+void lpuart_IRQHandler(void) {
+    if (lpuart_get_rx_not_empty_status(LPUART)) {
+        uint8_t byte1 = lpuart_receive_data(LPUART);
+
+        if (rx_index < RX_SIZE) {
+            rx_data[rx_index++] = byte1;
+        }else {
+            rx_index = 0; // reset on overflow (simple fix)
+			memset(rx_data, 0, RX_SIZE);
+        }
+    }
+}
+
 #ifdef USE_FULL_ASSERT
-void assert_failed(void* file, uint32_t line)
-{
+void assert_failed(void* file, uint32_t line) {
     (void)file;
     (void)line;
 
